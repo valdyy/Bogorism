c927b1ca5b02c1c862e82c94621d7173982c7038
Valdy Ramadhan <valdyramadhann@gmail.com>
2022-05-30 20:36:11 +0700

    Making a server on port 8000

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Bogorism
 Bogorism is an app that recommends tourist spots in the area.
+
+this project using express framework to create RESTFul API easily
+Used Modules :
+"mysql" to connect with MySQL 
+"body-parser" to parse parameter
+"crypto" to encrypt password
+"uuid" to create unique id
---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
+const crypto = require('crypto');
+const uuid = require('uuid');
+
+//Acc json params and acc URL encoded params
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({extended: true}));
+
+//Connect to DataBase
+const con = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'users'
+});
+
+const PORT = process.env.PORT || 8000
+app.listen(PORT, () => {
+    console.log("Server is up and listening on " + PORT)
+})
